23caf2b47543d2476cd8521051c00c354aa95ee0
RuslanZb <zubairov.rr@mail.ru>
2023-03-13 14:56:14 +0300

    5. PsqlStore [#285209]

---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,117 @@
+package ru.job4j.grabber;
+
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
+import ru.job4j.quartz.AlertRabbit;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store {
+
+    private Connection cnn;
+
+    public PsqlStore(Properties cfg) {
+        try {
+            Class.forName(cfg.getProperty("jdbc.driver"));
+            cnn = DriverManager.getConnection(
+                    cfg.getProperty("url"),
+                    cfg.getProperty("username"),
+                    cfg.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement ps = cnn.prepareStatement(
+                "insert into post(name, text, link, created) values (?, ?, ?, ?)" +
+                        "on conflict (link) do update set name = ?, text = ?, created = ?",
+                Statement.RETURN_GENERATED_KEYS
+        )) {
+            ps.setString(1, post.getTitle());
+            ps.setString(2, post.getDescription());
+            ps.setString(3, post.getLink());
+            ps.setTimestamp(4, Timestamp.valueOf(post.getLocalDateTime()));
+            ps.setString(5, post.getTitle());
+            ps.setString(6, post.getDescription());
+            ps.setTimestamp(7, Timestamp.valueOf(post.getLocalDateTime()));
+            ps.execute();
+            try (ResultSet generatedKey = ps.getGeneratedKeys()) {
+                if (generatedKey.next()) {
+                    post.setId(generatedKey.getInt(1));
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> posts = new ArrayList<>();
+        try (PreparedStatement ps = cnn.prepareStatement("select * from post")) {
+            try (ResultSet resultSet = ps.executeQuery()) {
+                while (resultSet.next()) {
+                    Post post = new Post();
+                    post.setId(resultSet.getInt("id"));
+                    post.setTitle(resultSet.getString("name"));
+                    post.setDescription(resultSet.getString("text"));
+                    post.setLink(resultSet.getString("link"));
+                    post.setLocalDateTime(resultSet.getTimestamp("created").toLocalDateTime());
+                    posts.add(post);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return posts;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post post = new Post();
+        try (PreparedStatement ps = cnn.prepareStatement("select * from post where id = ?")) {
+            ps.setInt(1, id);
+            try (ResultSet resultSet = ps.executeQuery()) {
+                while (resultSet.next()) {
+                    post.setId(resultSet.getInt("id"));
+                    post.setTitle(resultSet.getString("name"));
+                    post.setDescription(resultSet.getString("text"));
+                    post.setLink(resultSet.getString("link"));
+                    post.setLocalDateTime(resultSet.getTimestamp("created").toLocalDateTime());
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return post;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cnn != null) {
+            cnn.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Properties config = new Properties();
+        try (InputStream in = AlertRabbit.Rabbit.class.getClassLoader()
+                .getResourceAsStream("grabber.properties")) {
+            config.load(in);
+        }
+        Store store = new PsqlStore(config);
+        HabrCareerParse parse = new HabrCareerParse(new HabrCareerDateTimeParser());
+        List<Post> posts = parse.list("https://career.habr.com/vacancies/java_developer?page=");
+        for (Post post : posts) {
+            store.save(post);
+        }
+        System.out.println(store.findById(posts.get(0).getId()));
+    }
+}
