7b90822f712ff1947c49384a92938d41abc66d8e
RuslanZb <zubairov.rr@mail.ru>
2023-03-13 11:45:04 +0300

    2.4. HabrCareerParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,38 +5,60 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
 
-    private String retrieveDescription(String link) throws IOException {
-        Connection connection = Jsoup.connect(link);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-description__text");
+    private final DateTimeParser dateTimeParser;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
+    private static String retrieveDescription(String link){
+        Elements rows;
+        try {
+            Document document = Jsoup.connect(link).get();
+            rows = document.select(".vacancy-description__text");
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Exception in method 'retrieveDescription()'");
+        }
         return rows.text();
     }
 
-    public static void main(String[] args) throws IOException {
+    @Override
+    public List<Post> list(String link) throws IOException {
+        List<Post> posts = new ArrayList<>();
         for (int i = 1; i <= 5; i++) {
-            Connection connection = Jsoup.connect(String.format("%s%d", PAGE_LINK, i));
+            Connection connection = Jsoup.connect(String.format("%s%d", link, i));
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
             rows.forEach(row -> {
+                Post post = new Post();
                 Element dateElement = row.select(".vacancy-card__date").first().child(0);
                 Element titleElement = row.select(".vacancy-card__title").first();
                 Element linkElement = titleElement.child(0);
-                LocalDateTime vacancyDate = new HabrCareerDateTimeParser().parse(dateElement.attr("datetime"));
-                String vacancyName = titleElement.text();
-                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                System.out.printf("%s %s %s%n", vacancyName, link, vacancyDate);
+                post.setLocalDateTime(dateTimeParser.parse(dateElement.attr("datetime")));
+                post.setTitle(titleElement.text());
+                post.setLink(String.format("%s%s", SOURCE_LINK, linkElement.attr("href")));
+                post.setDescription(retrieveDescription(post.getLink()));
+                posts.add(post);
             });
         }
+        return posts;
+    }
+
+    public static void main(String[] args) throws IOException {
+        HabrCareerParse parse = new HabrCareerParse(new HabrCareerDateTimeParser());
+        System.out.println(parse.list(PAGE_LINK));
     }
 }
--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -10,6 +10,46 @@ public class Post {
     private String description;
     private LocalDateTime localDateTime;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
